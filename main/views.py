2766922305ddcd32cf77af67ff516319ae493cea
withyaww <wdyaac@gmail.com>
2024-11-13 16:53:33 +0700

    tutor 8

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
 
 import datetime
 from django.http import HttpResponse, HttpResponseRedirect
@@ -136,3 +138,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
